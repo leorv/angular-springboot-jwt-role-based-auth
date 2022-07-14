6975c8bc3ce6afc4478d96ab791080d7c1f8c6c1
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-07-13 23:30:54 -0300

    navbar and router links

--- jwt-ui/src/app/app-routing.module.ts
+++ jwt-ui/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'forbidden', component: ForbiddenComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'user-dashboard', component: UserComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- jwt-ui/src/app/app.module.ts
+++ jwt-ui/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    LoginComponent,
+    UserComponent,
+    HeaderComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,
